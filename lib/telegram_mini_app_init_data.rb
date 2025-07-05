89e10789c70aee913391a514185b82d34f6cf8c4
Artyom V. Peredery <peredery@pm.me>
2025-07-05 22:00:53 +0500

    feat(gem): init gem

---
+++ lib/telegram_mini_app_init_data.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "telegram_mini_app_init_data/version"
+
+module TelegramMiniAppInitData
+  class Error < StandardError; end
+  # Your code goes here...
+end
